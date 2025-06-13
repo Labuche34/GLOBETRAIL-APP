202ff2cb4cf8071cab979b0257d1485fa2dea2aa
Jimmy Beltran <beltranjimmy@outlook.com>
2025-06-13 16:20:51 +0200

    fini

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def home
     @travel = Travel.all.count
-    @stop
+    @stop = Stop.all.count
+    # @day = Travel.all.sum do |travel|
+    #   (travel.return_date - travel.departure_date).to_i + 1
+    # end
   end
-
 end
