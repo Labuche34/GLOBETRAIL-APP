2a49fa5f06b4c6f0299b5a957a0c177d5f094a1e
Lina FATHEDDINE <206774748+LGuen@users.noreply.github.com>
2025-06-16 18:09:32 +0200

    controller de spendings et create/new sur l'edit

--- app/controllers/spendings_controller.rb
+++ app/controllers/spendings_controller.rb
@@ -1,37 +1,37 @@
-class PicturesController < ApplicationController
+class SpendingsController < ApplicationController
   def index
     @travel = Travel.find(params[:travel_id])
     @stop = Stop.find(params[:stop_id])
-    @pictures = Picture.all
+    @spendings = Spending.all
     # sera sur la page visualisation de figma avec spendings et notes
   end
 
   def new
-    @picture = Picture.new
+    @spending = Spending.new
   end
 
   def create
     @travel = Travel.find(params[:travel_id])
     @stop = Stop.find(params[:stop_id])
-    @picture = Picture.new(picture_params)
+    @spending = Spending.new(spending_params)
     @stop.travel = @travel
-    @picture.stop = @stop
-    @picture.save
+    @spending.stop = @stop
+    @spending.save
     redirect_to travel_stop_pictures_path(@travel, @stop)
   end
 
   def edit
     @travel = Travel.find(params[:travel_id])
     @stop = Stop.find(params[:stop_id])
-    @picture = Picture.find(params[:id])
+    @spending = Spending.find(params[:id])
     # sera sur la page visualisation de figma avec spendings et notes
   end
 
   def update
     @travel = Travel.find(params[:travel_id])
     @stop = Stop.find(params[:stop_id])
-    @picture = Picture.find(params[:id])
-    if @picture.update(picture_params)
+    @spending = Spending.find(params[:id])
+    if @spending.update(spending_params)
       redirect_to stop_path(@stop)
     else
       render :edit, status: :unprocessable_entity
@@ -40,15 +40,15 @@ class PicturesController < ApplicationController
   end
 
   def destroy
-    @picture = Picture.find(params[:id])
-    @picture.destroy
-    redirect_to stop_path(@picture.stop), status: :see_other
+    @spending = Spending.find(params[:id])
+    @spending.destroy
+    redirect_to stop_path(@spending.stop), status: :see_other
     # sera sur la page visualisation de figma avec spendings et notes
   end
 
   private
 
-  def picture_params
-    params.require(:picture).permit(:description, photos: [])
+  def spending_params
+    params.require(:spending).permit(:category, :amount, :currency)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     resources :stops, only: [:create, :edit, :update] do
       resources :notes, only: [:index, :show, :new, :create, :edit, :update]
       resources :pictures, only: [:new, :create, :edit, :update, :index]
+      resources :spendings, only: [:new, :create, :edit, :update, :index]
     end
   end
 
@@ -24,6 +25,7 @@ Rails.application.routes.draw do
   resources :stops, only: [:destroy]
   resources :notes, only: [:destroy]
   resources :pictures, only: [:destroy]
+  resources :spendings, only: [:destroy]
 
   get "new_exploreo" => "travels#new_exploreo"
   post "create_exploreo" => "travels#create_exploreo"
