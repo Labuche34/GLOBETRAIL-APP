050b497a7571a5bb710fe5b658d6b71f07675599
Julie Boulard <juliefm1380@gmail.com>
2025-06-18 17:31:30 +0200

    meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   def default_url_options
-    { hôte : ENV["DOMAINE"] || "localhost :3000" }
+    { host : ENV["DOMAINE"] || "localhost :3000" }
   end
 end
