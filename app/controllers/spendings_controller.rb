b9557e1d44ac4cb5c0298b68fea389c3ba890339
Lina FATHEDDINE <206774748+LGuen@users.noreply.github.com>
2025-06-18 10:17:59 +0200

    spendings ajoutés et fonctionnels, manque le graph

--- app/controllers/spendings_controller.rb
+++ app/controllers/spendings_controller.rb
@@ -18,15 +18,15 @@ class SpendingsController < ApplicationController
     @spending.stop = @stop
 
     # Trouver la devise du pays
-    country = ISO3166::Country.new(@travel.country)
-    currency = country.currency.code rescue "EUR"
-    raise
+    # country = ISO3166::Country.new(@travel.country)
+    country = ISO3166::Country.find_country_by_any_name(@travel.country)  # => objet country
+    code = country&.alpha2                                            # => "ID", "US", "FR", etc.
+    currency = country.currency_code rescue "EUR"
     @spending.currency = currency
-
     # Convertir le montant vers l'EUR à la date d’aujourd’hui
     original_amount = Money.from_amount(@spending.amount.to_f, currency)
     converted = Money.default_bank.exchange_with(original_amount, "EUR")
-    @spending.amount_in_eur = converted.to_f
+    @spending.amount_in_eur_cents = (converted.to_f) * 100
 
     @spending.save
     redirect_to travel_stop_pictures_path(@travel, @stop), notice: "Spending added"
