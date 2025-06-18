da1b2227c258feac7b3368099ec4cbee162d862d
Julie Boulard <juliefm1380@gmail.com>
2025-06-18 17:53:13 +0200

    meta

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -4,7 +4,7 @@ content_for?(:meta_title) ? content_for(:meta_title) : DEFAULT_META["meta_title"
 end
 
 def meta_description
-content_for ? ( :meta_description) ? content_for( :meta_description) : fin DEFAULT_META["meta_description"]
+content_for?( :meta_description) ? content_for( :meta_description) : fin DEFAULT_META["meta_description"]
 
 def meta_image
 meta_image = (content_for ?( :meta_image) ? content_for( :meta_image) : DEFAULT_META["meta_image"])
