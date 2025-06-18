ff053bfe9aa5fe0e86cbd9ee0b5926d9b25002eb
Jean Loup <doc.jprs@gmail.com>
2025-06-18 12:36:14 +0200

    chart js setup

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -1,13 +1,42 @@
 import { Controller } from "@hotwired/stimulus"
+import Chart from 'chart.js/auto';
 
 // Connects to data-controller="chart"
 export default class extends Controller {
+
+  static targets = [ 'chart' ]
+
   static values = {
-    category: String,
-    spendings: String
+    category: Array,
+    spendings: Array
   }
+
   connect() {
-    console.log(this.categoryValue)
-    console.log(this.spendingsValue)
+    const ctx = this.chartTarget.getContext('2d');
+    this.chart = new Chart(ctx,
+    {
+      type: 'bar',
+      options: {
+        animation: false,
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            enabled: false
+          }
+        }
+      },
+      data: {
+        labels: this.categoryValue,
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: this.spendingsValue
+          }
+        ]
+      }
+    });
   }
+
 }
